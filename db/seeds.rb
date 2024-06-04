486df4d0d84f0375d2b0e217a82f990d1cb7a9d4
Ayax Abreu Garcia <ayax.abreu.garcia@gmail.com>
2024-06-04 15:05:44 +0200

    reworked seeds and added name to the events table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ require 'faker'
 
 
 puts 'Cleaning database, deleting users, events, donations and vaults...'
-Event.destroy_all
-Vault.destroy_all
-Donation.destroy_all
-User.destroy_all
+Donation.delete_all
+Event.delete_all
+Vault.delete_all
+User.delete_all
 
 puts 'Creating users'
 saul = User.create(email: 'saul@gmail.com', password: 'password', first_name: 'Saul', last_name: 'Goldman-Webb', profile_picture: 'https://as1.ftcdn.net/v2/jpg/04/00/78/46/1000_F_400784687_nevcbFY2grwEA3wdAu2asJvx1GLFg0EJ.jpg')
@@ -24,7 +24,7 @@ puts 'creating vaults'
 
 puts 'Creating events'
 16.times do |d|
-Event.create(goal_amount: rand(150..1000), current_amount: 0,
+Event.create(name: Faker::Quotes::Shakespeare.hamlet_quote, goal_amount: rand(150..1000), current_amount: 0,
   picture: "https://images.adsttc.com/media/images/5b7c/2b69/f197/ccd0/fb00/0052/newsletter/MIRA_Superman_Hero_Courtesy_Studio_Gang_and_Binyan.tif.jpg?1534864224",
   description: Faker::Company.bs, location: Faker::Address.full_address, vault_id: rand(1..3), user_id: rand(1..3))
 end
