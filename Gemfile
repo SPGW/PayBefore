8a242a3304f6e6855229754e9a01422d548b3773
Ayax Abreu Garcia <ayax.abreu.garcia@gmail.com>
2024-06-04 14:42:57 +0200

    created seed file without name for events

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.1.2"
 #added by us (team)
 gem "devise"
 gem "cloudinary"
+gem 'faker'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.3"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,37 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+
+# also part of test
+require "open-uri"
+require 'faker'
+
+
+puts 'Cleaning database, deleting users, events, donations and vaults...'
+Event.destroy_all
+Vault.destroy_all
+Donation.destroy_all
+User.destroy_all
+
+puts 'Creating users'
+saul = User.create(email: 'saul@gmail.com', password: 'password', first_name: 'Saul', last_name: 'Goldman-Webb', profile_picture: 'https://as1.ftcdn.net/v2/jpg/04/00/78/46/1000_F_400784687_nevcbFY2grwEA3wdAu2asJvx1GLFg0EJ.jpg')
+ayax = User.create(email: 'ayax@gmail.com', password: 'password', first_name: 'Ayax', last_name: 'Abreu Garcia', profile_picture: 'https://as1.ftcdn.net/v2/jpg/04/00/78/46/1000_F_400784687_nevcbFY2grwEA3wdAu2asJvx1GLFg0EJ.jpg')
+shex = User.create(email: 'shex@gmail.com', password: 'password', first_name: 'Ezzeldin', last_name: 'Osman', profile_picture: 'https://as1.ftcdn.net/v2/jpg/04/00/78/46/1000_F_400784687_nevcbFY2grwEA3wdAu2asJvx1GLFg0EJ.jpg')
+
+
+puts 'creating vaults'
+  Vault.create(donation_amount: 0, current_amount_vault:0, name: "Housing")
+  Vault.create(donation_amount: 0, current_amount_vault:0, name: "Medical")
+  Vault.create(donation_amount: 0, current_amount_vault:0, name: "Education")
+
+puts 'Creating events'
+16.times do |d|
+Event.create(goal_amount: rand(150..1000), current_amount: 0,
+  picture: "https://images.adsttc.com/media/images/5b7c/2b69/f197/ccd0/fb00/0052/newsletter/MIRA_Superman_Hero_Courtesy_Studio_Gang_and_Binyan.tif.jpg?1534864224",
+  description: Faker::Company.bs, location: Faker::Address.full_address, vault_id: rand(1..3), user_id: rand(1..3))
+end
+
+puts 'creating donations'
+5.times do |d|
+  Donation.create(user_id: rand(1..3), contribution: rand(10..50), event_id: rand(1..5), vault_id: rand(1..3))
+end
+
+puts 'Finished mate'
