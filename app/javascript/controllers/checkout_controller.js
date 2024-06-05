5f1545e4633618ff18068aafd588362e8e70abdd
SaulPatrickGoldmanWebb <spgw00@gmail.com>
2024-06-05 17:55:21 +0200

    more stripe

--- app/javascript/controllers/checkout_controller.js
+++ app/javascript/controllers/checkout_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="checkout"
 export default class extends Controller {
+  static targets = ["payment", "submit", "error"]
   static values = {
     secret: String,
     key: String
@@ -9,5 +10,16 @@ export default class extends Controller {
   connect() {
     console.log(this.secretValue)
     console.log(this.keyValue)
+    this.stripe = Stripe(this.keyValue);
+    const options = {
+      clientSecret: this.secretValue,
+      // Fully customizable with appearance API.
+      // appearance: {/*...*/},
+    };
+    const elements = this.stripe.elements(options);
+
+    // Create and mount the Payment Element
+    const paymentElement = elements.create('payment');
+    paymentElement.mount(this.paymentTarget);
   }
 }
